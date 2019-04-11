68e743b1d317d8521132aff3c16af94ac14802b4
guisteink <39563063+guisteink@users.noreply.github.com>
2019-04-11 09:43:06 -0300

    Update Uso.java

--- Exercises/Uso.java
+++ Exercises/Uso.java
@@ -8,12 +8,13 @@ public class Uso {
         // data2 g = new data2("01/01/1900");
         // data2 g = new data2(01, "janeiro", 1900);
         // data2 g = new data2(01, 1900);
-        int dia = Integer.parseInt(JOptionPane.showInputDialog("Dia: "));
-        String mes = JOptionPane.showInputDialog("Mes: ");
-        //int z = converte(mes);
-        int ano = Integer.parseInt(JOptionPane.showInputDialog("Ano: "));
-        Data g = new Data(dia, mes, ano);
-        g.setData(dia, mes , ano);
+        
+        String dataxx = JOptionPane.showInputDialog("Digite a data: ");
+        
+        //int dia = Integer.parseInt(JOptionPane.showInputDialog("Dia: "));
+        //String mes = JOptionPane.showInputDialog("Mes: ");
+        //int ano = Integer.parseInt(JOptionPane.showInputDialog("Ano: "));
+        Data g = new Data(dataxx);
  
         System.out.println(g.toString());
     }
