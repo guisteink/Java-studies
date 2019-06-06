a0f9fc4ef0fd273077606ed71f1d6c1506faff31
Guilherme Stein Kuhn <39563063+guisteink@users.noreply.github.com>
2019-06-05 23:46:35 -0300

    Add files via upload

---
+++ Exercises/Ex21/ConstantesMatematicas.java
@@ -0,0 +1,12 @@
+package Ex21;
+
+interface ConstantesMatematicas {
+	double RAIZDE2 = 1.41421356237309;
+	double RAIZDE3 = 1.73205080756887;
+	double RAIZDE5 = 2.23606797749978;
+	double RAIZDE6 = RAIZDE2 * RAIZDE3;
+	
+}
+
+
+
