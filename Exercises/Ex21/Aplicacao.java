0633393cdb514f6df9759f7e11676dd944179e42
Guilherme Stein Kuhn <39563063+guisteink@users.noreply.github.com>
2019-06-05 23:46:15 -0300

    Create Aplicacao.java

---
+++ Exercises/Ex21/Aplicacao.java
@@ -0,0 +1,9 @@
+package Ex21;
+
+class Aplicacao implements ConstantesMatematicas {
+	public static void main(String[] args) {
+		StringBuilder output = new StringBuilder();
+		output.append("\nraiz de 2: " + ConstantesMatematicas.RAIZDE2 + "\nraiz de 3: " + ConstantesMatematicas.RAIZDE3
+				+ "\nraiz de 5: " + ConstantesMatematicas.RAIZDE5 + "\nraiz de 6: " + ConstantesMatematicas.RAIZDE6);
+	}
+}
